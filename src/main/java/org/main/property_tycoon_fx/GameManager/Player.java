11d172ebcdf46d2cc28d5eab32b31d3e8fe4b4a1
K Scott <165611046+betterthanamasterofone@users.noreply.github.com>
2025-04-11 01:21:01 +0100

    Update Player.java
    Added GOOJ - kept check condition as void

--- src/main/java/org/main/property_tycoon_fx/GameManager/Player.java
+++ src/main/java/org/main/property_tycoon_fx/GameManager/Player.java
@@ -5,24 +5,35 @@ import javafx.scene.image.ImageView;
 import javafx.scene.control.Button;
 import java.util.LinkedList;
 
-
 public class Player {
     private int playerID;
     private String playerName;
     private int Money;
     private String token;
 
-    // to access dice
-    private GameBoard gameBoard;    // let player reference gameboard
+    private GameBoard gameBoard;
     private Dice dice;
     private Button rollButton;
 
-      private int tilePosition = 0;
-      private int minimumPosition = 0;
-      private int maxPosition = 39;
+    private int tilePosition = 0;
+    private int minimumPosition = 0;
+    private int maxPosition = 39;
 
+    private ImageView playerTokenImage;
 
-     // Tile CurrentTile;
+    //Jail + GOOJ system
+    private boolean isInJail = false;
+    private int jailTurns = 0;
+    private int getOutOfJailCards = 0;
+
+    public Player(int playerID, String playerName, int Money, GameBoard gameBoard) {
+        this.Money = Money;
+        this.playerID = playerID;
+        this.playerName = playerName;
+        this.gameBoard = gameBoard;
+        dice = gameBoard.getDice();
+        token = gameBoard.giveToken();
+    }
 
     public String getPlayerName() {
         return playerName;
@@ -44,37 +55,28 @@ public class Player {
         return Money;
     }
 
-    public ImageView getPlayerTokenImage() {
-        return playerTokenImage;
-    }
-
-    public ImageView playerTokenImage;
-
-
     public void setMoney(int money) {
         Money = money;
     }
-
-    public void setTilePosition(int tilePosition) {this.tilePosition = tilePosition;}
 
     public int getTilePosition() {
         return tilePosition;
     }
 
-
-    public Player(int playerID, String playerName, int Money, GameBoard gameBoard) {
-        this.Money = Money;
-        this.playerID = playerID;
-        this.playerName = playerName;
-        this.gameBoard = gameBoard;
-        dice = gameBoard.getDice();    // get dice object created in gameboard
-        token = gameBoard.giveToken();
+    public void setTilePosition(int tilePosition) {
+        this.tilePosition = tilePosition;
     }
 
+    public ImageView getPlayerTokenImage() {
+        return playerTokenImage;
+    }
+
+    public GameBoard getGameBoard() {
+        return gameBoard;
+    }
 
     public ImageView playerImageView() {
         System.out.println(playerName + " is: " + token);
-        // create image and imageview
         Image tokenImage = new Image("/images/" + token + "_token.png", 60, 60, true, true);
         ImageView imageView = new ImageView(tokenImage);
         playerTokenImage = imageView;
@@ -83,22 +85,19 @@ public class Player {
 
     public ImageView playerTabImageView() {
         System.out.println(playerName + " is tab: " + token);
-        // create image and IV
         Image tabImage = new Image("/images/" + token + "_tab.png", 328, 0, true, true);
         ImageView tabImageView = new ImageView(tabImage);
         return tabImageView;
     }
 
-     public int move()
-     {
-         if (dice.canRollAgain()) {
-             int moveValue = dice.rollDice();
-             tilePosition = tilePosition + moveValue;
-
-             System.out.println(playerName + " position: " + tilePosition);
-         } else{
-             System.out.println(playerName + " can't roll again");
-         }
+    public int move() {
+        if (dice.canRollAgain()) {
+            int moveValue = dice.rollDice();
+            tilePosition = tilePosition + moveValue;
+            System.out.println(playerName + " position: " + tilePosition);
+        } else {
+            System.out.println(playerName + " can't roll again");
+        }
 
         if (tilePosition > maxPosition) {
             int difference = tilePosition - maxPosition;
@@ -106,50 +105,122 @@ public class Player {
         }
 
         System.out.println(tilePosition);
-        CheckCondition();
         return tilePosition;
-     }
+    }
 
-
-
-
-    public void buyTileProperty(int tilePosition)
-    {
-        // Get List of tiles from Excel
+    public void buyTileProperty(int tilePosition, Bank bank) {
         TileReader TReader = new TileReader();
-        TReader.getTileDetails(); // Load tile details from Excel
-        LinkedList<Tile> tileList = TReader.returnTileList(); // Get tiles from TileReader
+        TReader.getTileDetails();
+        LinkedList<Tile> tileList = TReader.returnTileList();
         Tile currentTile = tileList.get(tilePosition);
-        if(currentTile.isCanBeBought())
-        {
-            if (this.Money >= currentTile.getCost())
-            {
-              this.Money -= (int) currentTile.getCost();
-              // The thing to make it like update on excel
-                System.out.println("Cha Ching, Bought "+ currentTile.getSpace());
+
+        if (currentTile.isCanBeBought()) {
+            int cost = (int) currentTile.getCost();
+            if (getMoney() >= cost) {
+                bank.chargePlayer(this, cost, "Property purchase: " + currentTile.getSpace());
                 currentTile.setCanBeBought(false);
-                currentTile.setIsOwnedBy(this.playerID);
-                System.out.println(currentTile.getSpace() + " Is owned by " + currentTile.getIsOwnedBy());
-            }
-            else
-            {
-                System.out.println("You Broke or tile no can be bought lol");
+                currentTile.setIsOwnedBy(this.getPlayerID());
+                System.out.println("Cha Ching, Bought " + currentTile.getSpace());
+            } else {
+                System.out.println("You're broke, can't buy " + currentTile.getSpace());
             }
         }
+    }
+
+    public void takeTurn() {
+        // Default or placeholder behavior for human players
+        System.out.println(getPlayerName() + "'s turn.");
+    }
+
+    // Jail!!!
+    public boolean isInJail() {
+        return isInJail;
+    }
+
+    public void setInJail(boolean inJail) {
+        isInJail = inJail;
+        if (!inJail) jailTurns = 0;
+    }
+
+    public int getJailTurns() {
+        return jailTurns;
+    }
+
+    public void incrementJailTurns() {
+        jailTurns++;
+    }
+
+    public void resetJailTurns() {
+        jailTurns = 0;
+    }
+
+    public void moveToJail() {
+        System.out.println(getPlayerName() + " is sent to Jail.");
+        setTilePosition(10);
+        setInJail(true);
+        resetJailTurns();
+        getGameBoard().passPlayerPosTile(this, 10);
+    }
+
+    public boolean handleJailTurn() {
+        if (!isInJail()) return false;
+
+        System.out.println(getPlayerName() + " is in jail (Turn " + getJailTurns() + "/3)");
+
+        if (hasGetOutOfJailCard()) {
+            System.out.println(getPlayerName() + " used a Get Out of Jail Free card.");
+            useGetOutOfJailCard();
+            setInJail(false);
+            resetJailTurns();
+            return false;
+        }
+
+        incrementJailTurns();
+
+        if (getJailTurns() >= 3) {
+            System.out.println(getPlayerName() + " has served their sentence and is released.");
+            setInJail(false);
+            resetJailTurns();
+            return false;
+        }
+
+        System.out.println(getPlayerName() + " skips this turn.");
+        return true;
+    }
+
+    // GOOJ Cards
+    public int getGetOutOfJailCards() {
+        return getOutOfJailCards;
+    }
+
+    public void addGetOutOfJailCard() {
+        this.getOutOfJailCards++;
+    }
+
+    public void useGetOutOfJailCard() {
+        if (getOutOfJailCards > 0) {
+            this.getOutOfJailCards--;
+        }
+    }
+
+    public boolean hasGetOutOfJailCard() {
+        return getOutOfJailCards > 0;
     }
 
     public void CheckCondition() {
         if (tilePosition == 3 || tilePosition == 18 || tilePosition == 34) {
 
             // reference to popup class
-       //     Popup popup = new Popup("PotLuck", 0);
-         //   gameBoard.addPopup(popup.getPopupBox());
+            //     Popup popup = new Popup("PotLuck", 0);
+            //   gameBoard.addPopup(popup.getPopupBox());
         }
         if (tilePosition == 8 || tilePosition == 23 || tilePosition == 37) {
             // reference to popup class
-         //   Popup popup = new Popup("OppKnocks", 0);
-           // gameBoard.addPopup(popup.getPopupBox());
+            //   Popup popup = new Popup("OppKnocks", 0);
+            // gameBoard.addPopup(popup.getPopupBox());
         }
     }
 
 }
+
+
